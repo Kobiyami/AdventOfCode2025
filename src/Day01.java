3603d9c77e6d4ebb57e8a425e2899e23b456952d
Kobiyami <fournierg01@gmail.com>
2025-12-04 13:56:15 +0100

    input read correctly

--- src/Day01.java
+++ src/Day01.java
@@ -1,19 +1,28 @@
-public class Day01 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-    public static void main(String[] args) {
+public class Day01 {
+public static void main(String[] args) {
         System.out.println("AdventOfCode Day 01 - first step");
 
         // PSEUDO-CODE :
         // On initialise la variable dial à 50
+        int dial = 50;
         // On initialise une variable password à 0
+        int password = 0;
         // On parcourt toutes les lignes de l'input
-        //     On transforme la ligne en chaine de caractères si nécessaire
-        //     Si le premier caractère de la chaine est un L :
-        //         on soustrait le reste de la chaine (converti en integer) à dial
-        //     Sinon :
-        //         on ajoute le reste de la chaine (converti en integer) à dial
-        //     Si la valeur absolue de dial est un multiple de 100 :
-        //         on incrémente password
-        // On affiche password
+   String filename = "inputs/day01.txt";
+    try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
+        String line;
+        while ((line = br.readLine()) != null) {
+
+            System.out.println(line);
+        }
+
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+}
 }
