88fe5f4d70fe0e0d2695215bbe370b80042e7a64
Kobiyami <fournierg01@gmail.com>
2025-12-04 10:32:36 +0100

    Day01Step1 Pseudocode

--- src/Day01.java
+++ src/Day01.java
@@ -1,9 +1,19 @@
 public class Day01 {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        System.out.println("AdventOfCode Day 01 - first step");
+
+        // PSEUDO-CODE :
+        // On initialise la variable dial à 50
+        // On initialise une variable password à 0
+        // On parcourt toutes les lignes de l'input
+        //     On transforme la ligne en chaine de caractères si nécessaire
+        //     Si le premier caractère de la chaine est un L :
+        //         on soustrait le reste de la chaine (converti en integer) à dial
+        //     Sinon :
+        //         on ajoute le reste de la chaine (converti en integer) à dial
+        //     Si la valeur absolue de dial est un multiple de 100 :
+        //         on incrémente password
+        // On affiche password
     }
 }
